c6fdb00bd07cdcfba702f0c5ac747d75773f4bd6
Md Mubin <fontdevmubin@gmail.com>
2025-03-31 10:25:21 +0600

    Changes to be committed: 	modified:   src/Commons/CommonReviewCards.jsx

--- src/Commons/CommonReviewCards.jsx
+++ src/Commons/CommonReviewCards.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const CommonReviewCards = ({commonReviewHead,commonReviewInfo}) => {
     return (
         <>
-            <ul className='h-[350px] px-[30px] pt-[48px] pb-[23px] rounded-2xl text-center shadow-[2px_2px_8px_#000] hover:bg-[#d2e8fd] hover:translate-y-[-5px] duration-200'>
+            <ul className='h-[90%] 2xl:h-[80%] px-[30px] pt-[48px] pb-[23px] rounded-2xl text-center shadow-[2px_2px_8px_#000] hover:bg-[#d2e8fd] hover:translate-y-[-5px] duration-200'>
                 <li className='w-[80px] h-[80px] m-auto bg-[#f0f0f0] rounded-full'>
                     <img src="#" alt="client_image" />
                 </li>
 
-                <li className='my-6 font-bold text-[34px] text-[#484A61] leading-[30px]'>
+                <li className='my-6 font-bold text-3xl text-[#484A61] leading-[30px]'>
                     {commonReviewHead}
                 </li>
 
-                <li className='font-normal text-xl text-[#484A61] leading-[42px]'>
+                <li className='font-normal text-[16px] xl:text-lg text-[#484A61] 2xl:leading-[32px]'>
                     {commonReviewInfo}
                 </li>
             </ul>
