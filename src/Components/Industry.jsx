5c5a215427d9440071750edc126ed798052fb649
Md Mubin <fontdevmubin@gmail.com>
2025-04-02 11:49:30 +0600

    Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/Components/Industry.jsx

--- src/Components/Industry.jsx
+++ src/Components/Industry.jsx
@@ -30,7 +30,7 @@ const Industry = () => {
                                     </li>
                                     <li>
                                         <h3 className="text-lg font-semibold text-gray-800 group-hover:text-[#fff] duration-200">{category.title}</h3>
-                                        <p className="text-gray-500 text-sm group-hover:text-[#fff] duration-200">{category.positions} open positions</p>
+                                        <p className="text-gray-500 text-sm group-hover:text-[#fff] duration-200 text-start">{category.positions} open positions</p>
                                     </li>
                                 </ul>
                                 
