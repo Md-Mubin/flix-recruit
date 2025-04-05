4fef8eb2183c891a10f9a2784631744008d011a7
Md Mubin <fontdevmubin@gmail.com>
2025-04-06 02:23:38 +0600

    Changes to be committed: 	modified:   src/Components/Counter.jsx 	modified:   src/Components/Navbar.jsx

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -7,13 +7,13 @@ const Counter = () => {
             <section className='py-10 bg-[#d2e8fd]'>
                 <div className="container">
                     <ul className='text-center flex flex-wrap lg:flex-nowrap justify-center items-center lg:justify-between gap-10 lg:gap-0'>
-                        <li className='font-bold text-[56px] text-[#000] flex flex-col'>
+                        <li className='font-bold text-[56px] text-[#000] flex flex-col '>
                             <SlotCounter value={[
                                 <span>1</span>,
                                 <span>2</span>,
                                 <span>K</span>
                             ]} duration={2} />
-                            <span className='font-normal text-[36px]'>
+                            <span className='font-normal text-[28px] tracking-widest'>
                                 Happy Client
                             </span>
                         </li>
@@ -24,7 +24,7 @@ const Counter = () => {
                                 <span>0</span>,
                                 <span>K</span>
                             ]} duration={2} />
-                            <span className='font-normal text-[36px]'>
+                            <span className='font-normal text-[28px] tracking-widest'>
                                 Project Done
                             </span>
                         </li>
@@ -38,7 +38,7 @@ const Counter = () => {
                                 ]} duration={2} />
                                 <span className='text-3xl'>%</span>
                             </div>
-                            <span className='font-normal text-[36px]'>
+                            <span className='font-normal text-[28px] tracking-widest'>
                                 Satisfied User
                             </span>
                         </li>
@@ -48,7 +48,7 @@ const Counter = () => {
                                 <span>0</span>,
                                 <span>8</span>
                             ]} duration={2} />
-                            <span className='font-normal text-[36px]'>
+                            <span className='font-normal text-[28px] tracking-widest'>
                                 Operated Years
                             </span>
                         </li>
