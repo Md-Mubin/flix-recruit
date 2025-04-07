533bc2964a247844d857d7e302cec8ed3f2a826a
Md Mubin <fontdevmubin@gmail.com>
2025-04-07 12:27:13 +0600

    Changes to be committed: 	modified:   src/Commons/CommonReviewCards.jsx 	modified:   src/Components/Industry.jsx 	modified:   src/app/globals.css

--- src/Commons/CommonReviewCards.jsx
+++ src/Commons/CommonReviewCards.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { RiDoubleQuotesR } from "react-icons/ri"
 
-const CommonReviewCards = ({commonReviewHead,commonReviewInfo}) => {
+const CommonReviewCards = ({ commonReviewHead, commonReviewInfo }) => {
     return (
         <>
-            <ul className='h-[80%] px-[30px] pt-[48px] pb-[23px] rounded-2xl text-center shadow-[2px_2px_8px_#000] hover:bg-[#d2e8fd] hover:translate-y-[-5px] duration-200 cursor-grab active:cursor-grabbing'>
-                <li className='w-[80px] h-[80px] m-auto bg-[#f0f0f0] rounded-full'>
-                    <img src="#" alt="client_image" />
+            <ul class="w-full p-6 rounded-2xl shadow-md hover:shadow-lg transition duration-300 cursor-grab active:cursor-grabbing">
+                <li class="flex items-center gap-10 mb-4">
+                    <img src="#" class="w-20 h-20 rounded-full bg-[#d2e8fd]" alt="Client" />
+                    <div>
+                        <h4 class="text-xl font-semibold text-gray-800">Anna Müller</h4>
+                        <p class="text-lg text-gray-500">Founder, NordicTech</p>
+                    </div>
                 </li>
-
-                <li className='my-6 font-bold text-3xl text-[#484A61] leading-[30px] tracking-widest'>
-                    {commonReviewHead}
-                </li>
-
-                <li className='font-medium text-[16px] xl:text-lg text-[#484A61] 2xl:leading-[32px] tracking-widest'>
+                <li class="h-[280px] px-4 pt-10 mt-10 rounded-lg text-gray-600 leading-relaxed flex flex-col bg-[#d2e8fd80] tracking-widest relative">
+                    <span className='font-semibold text-[28px] mb-4 tracking-widest'>{commonReviewHead}</span>
                     {commonReviewInfo}
+                    <span className='absolute text-4xl p-4 bg-[#3971ca] text-[#fff] rounded-2xl top-[-30px] right-6'>
+                        <RiDoubleQuotesR/>
+                    </span>
                 </li>
             </ul>
         </>
--- src/Components/Industry.jsx
+++ src/Components/Industry.jsx
@@ -24,12 +24,12 @@ const Industry = () => {
                     <div className="m-auto flex flex-wrap justify-center gap-6 mt-10">
                         {
                             jobCategories.map((category, index) => (
-                                <ul key={index} className="w-[420px] flex items-center gap-4 p-6 bg-white rounded-2xl shadow-[0_2px_8px_rgba(0,0,0,0.05)] hover:shadow-lg hover:bg-[#95b7e5] group transition duration-200 ">
-                                    <li className="text-3xl bg-[#d2e8fd] w-[60px] h-[60px] rounded-sm flex items-center justify-center group-hover:bg-[#fff]">
+                                <ul key={index} className="w-[420px] flex items-center gap-4 p-6 bg-white rounded-2xl shadow-[0_2px_8px_rgba(0,0,0,0.05)] hover:shadow-none hover:bg-[#95b7e5] group transition duration-200 ">
+                                    <li className="text-3xl bg-gray-100 w-[80px] h-[60px] rounded-sm flex items-center justify-center group-hover:bg-[#fff]">
                                         {category.icon}
                                     </li>
                                     <li className='w-full text-start'>
-                                        <h3 className="text-lg font-semibold text-gray-800 group-hover:text-[#fff] duration-200 tracking-widest">{category.title}</h3>
+                                        <h3 className="text-lg font-semibold text-gray-900 group-hover:text-[#fff] duration-200 tracking-widest">{category.title}</h3>
                                         <p className="text-gray-500 text-sm group-hover:text-[#fff] duration-200 text-start tracking-widest">{category.positions} open positions</p>
                                     </li>
                                 </ul>
