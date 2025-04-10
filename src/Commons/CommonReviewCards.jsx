7546a93b86ec8fa58c9601aa1da0271de29c6a29
Md Mubin <fontdevmubin@gmail.com>
2025-04-10 20:47:55 +0600

    Changes to be committed: 	modified:   src/Commons/CommonReviewCards.jsx

--- src/Commons/CommonReviewCards.jsx
+++ src/Commons/CommonReviewCards.jsx
@@ -4,15 +4,15 @@ import { RiDoubleQuotesR } from "react-icons/ri"
 const CommonReviewCards = ({ commonReviewHead, commonReviewInfo }) => {
     return (
         <>
-            <ul class="w-full p-6 rounded-2xl shadow-md hover:shadow-lg transition duration-300 cursor-grab active:cursor-grabbing">
-                <li class="flex items-center gap-10 mb-4">
-                    <img src="#" class="w-20 h-20 rounded-full bg-[#d2e8fd]" alt="Client" />
+            <ul className="w-full p-6 rounded-2xl shadow-md hover:shadow-lg transition duration-300 cursor-grab active:cursor-grabbing">
+                <li className="flex items-center gap-10 mb-4">
+                    <img src="#" className="w-20 h-20 rounded-full bg-[#d2e8fd]" alt="Client" />
                     <div>
-                        <h4 class="text-xl font-semibold text-gray-800">Anna Müller</h4>
-                        <p class="text-lg text-gray-500">Founder, NordicTech</p>
+                        <h4 className="text-xl font-semibold text-gray-800">Anna Müller</h4>
+                        <p className="text-lg text-gray-500">Founder, NordicTech</p>
                     </div>
                 </li>
-                <li class="h-[280px] px-4 pt-10 mt-10 rounded-lg text-gray-600 leading-relaxed flex flex-col bg-[#d2e8fd80] tracking-widest relative">
+                <li className="h-[280px] px-4 pt-10 mt-10 rounded-lg text-gray-600 leading-relaxed flex flex-col bg-[#d2e8fd80] tracking-widest relative">
                     <span className='font-semibold text-[28px] mb-4 tracking-widest'>{commonReviewHead}</span>
                     {commonReviewInfo}
                     <span className='absolute text-4xl p-4 bg-[#3971ca] text-[#fff] rounded-2xl top-[-30px] right-6'>
